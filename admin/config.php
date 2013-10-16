78666d614b07be975f5e770f9de51d4130410630
Ariunbayar Baterdene <1480970+ariunbayar@users.noreply.github.com>
2013-10-16 13:28:02 +0800

    development database config

--- admin/config.php
+++ admin/config.php
@@ -2,9 +2,9 @@
 // Database Settings
 define('DB_HOST','127.0.0.1'); 	  // host
 define('DB_USER','root'); 	  // username
-define('DB_PASSWORD','apmsetup'); // password
+define('DB_PASSWORD',''); // password
 define('DB_NAME','cloaker');	  // database name
 
 // Application Settings
-define('ADMIN_URL','http://local.cloaker.test/admin/'); // full URL to the Admin Panel
+define('ADMIN_URL','http://cloaker.dev/admin/'); // full URL to the Admin Panel
 ?>
