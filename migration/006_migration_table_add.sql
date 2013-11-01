0572c6634ce1ceffd181336d3ad5717ba9b94cac
atulga <atuwow>
2013-11-01 08:44:08 +0800

    I2: 006_migration_table_add.sql insert values

--- migration/006_migration_table_add.sql
+++ migration/006_migration_table_add.sql
@@ -4,4 +4,11 @@ CREATE TABLE IF NOT EXISTS `migration` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
-INSERT INTO `cloaker`.`migration` (`id`, `file_name`) VALUES (NULL, '006_migration_table_add.sql');
+
+INSERT INTO `migration` (`id`, `file_name`) VALUES
+(1, '001_database_dump.sql'),
+(2, '002_add_traffic_source_and_affiliate_network_campaign.sql'),
+(3, '003_add_user_id_field_in_traffic_affiliate.sql'),
+(4, '004_new_fields_to_campaign.sql'),
+(5, '005_traffic_source_and_network_fix.sql'),
+(6, '006_migration_table_add.sql');
