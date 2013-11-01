3bba69d00b4b567eb0b83f320e72ae09cd9b402f
atulga <atuwow>
2013-11-01 10:27:07 +0800

    I4: delete sample datamigration added

---
+++ migration/007_delete_sample_migration.sql
@@ -0,0 +1,2 @@
+DELETE FROM migration WHERE file_name = 'sample_data_cleanup.sql';
+DELETE FROM migration WHERE file_name = 'sample_iptracker_data_for_campaign_999.sql';
--- migration/sample_data_cleanup.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM iptracker WHERE campaign_id=999;
-DELETE FROM destinations WHERE campaign_id=999;
-DELETE FROM campaigns WHERE id=999;
